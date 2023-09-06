cba50fdbc8bb4b5354d9edf0f0fb64bd9939f1a5
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2023-09-06 15:06:21 +0700

    added some configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,15 +23,20 @@ const MY_REPO = "/my-blog-v2";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
+let base = undefined;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
 }
 
+if (isBuild) {
+  base = MY_REPO;
+}
+
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
-  base: MY_REPO,
+  base,
   integrations: [
     sitemap(),
     tailwind({
