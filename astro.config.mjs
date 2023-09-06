6c35578a4ea25e391a54c67f08d39fb950a2627d
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2023-09-06 15:03:09 +0700

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,8 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://brynghiffar.github.io";
+const MY_REPO = "/my-blog-v2";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -30,6 +31,7 @@ if (isBuild) {
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
+  // base: MY_REPO,
   integrations: [
     sitemap(),
     tailwind({
